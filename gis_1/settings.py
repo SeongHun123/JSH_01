e6256f5a511361cea5c21b9ec2d3b84848e6ad8b
JSH <vcx2079@naver.com>
2021-07-05 14:33:51 +0900

    07/05 Web class  commit1

--- gis_1/settings.py
+++ gis_1/settings.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env_list=dict()
+env_list = dict()
 
 local_env=open(os.path.join(BASE_DIR,".env"))
 
@@ -33,7 +33,7 @@ while True:
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =env_list("SECRET_KEY")
+SECRET_KEY = env_list["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
