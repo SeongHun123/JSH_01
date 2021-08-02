bb2fdcf473a5cc5d73c3abbdcddd13129bd66544
JSH <vcx2079@naver.com>
2021-08-02 15:48:23 +0900

    8/02 commit 3

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from articleapp.forms import ArticleCreationForm
+from articleapp.models import Article
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreationForm
+    success_url = reverse_lazy('account:app')
+
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        return super().form_valid(form)
