445b247a3e65bcf56faf6a109db048a09df37e78
JSH <vcx2079@naver.com>
2021-08-09 14:49:07 +0900

    8 09 commit01

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.template.defaulttags import comment
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from commentapp.forms import CommentCreationForm
+
+
+class CommentCreateView(CreateView):
+    model = comment
+    form_class = CommentCreationForm
+
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
