a289be95640f6a198d1badfda744a3fe5b40257b
JSH <vcx2079@naver.com>
2021-08-09 15:40:46 +0900

    8 09 commit 04

--- commentapp/views.py
+++ commentapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.template.defaulttags import comment
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from articleapp.models import Article
 from commentapp.forms import CommentCreationForm
@@ -18,6 +18,13 @@ class CommentCreateView(CreateView):
         form.instance.writer = self.request.user
         form.instance.article_id = self.request.POST.get('article_pk')
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+
+class CommentDeleteView(DeleteView):
+    model = comment
+    context_object_name = 'target_comment'
 
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
